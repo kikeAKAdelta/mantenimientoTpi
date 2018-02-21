f483c010d36ec7a24481e5e8f419718a8c159c77
Enrique Arevalo <kike04arevalo@gmail.com>
2018-02-20 18:02:30 -0600

    agregando clase CargarArchivos

---
+++ src/main/java/com/mycompany/mantenimientodatalib/ProcesadorArchivos.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.mantenimientodatalib;
+
+import java.io.Serializable;
+
+
+public class ProcesadorArchivos implements Serializable {
+    
+    public String saludo(){
+        return "Hola bitch";
+    }
+    
+}
